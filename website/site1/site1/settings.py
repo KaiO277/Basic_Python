ba89da3217496bb9f6a708fc561ee9a0ea26d19f
KaiO277 <nghiatran1527@gmail.com>
2023-05-29 16:27:53 +0700

    KaiO

--- website/site1/site1/settings.py
+++ website/site1/site1/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'app1',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,13 @@ WSGI_APPLICATION = 'site1.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "site1",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        
     }
 }
 
