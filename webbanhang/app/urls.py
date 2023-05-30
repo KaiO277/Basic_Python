63c1e7f809f2bfdb227a2b7af86b3d1475addf17
KaiO277 <nghiatran1527@gmail.com>
2023-05-30 08:54:12 +0700

    KaiO

--- webbanhang/app/urls.py
+++ webbanhang/app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
+    path('cart/',views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
--- webbanhang/app/views.py
+++ webbanhang/app/views.py
@@ -4,6 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    context = {}
+    return render(request, 'app/home.html', context)
+def cart(request):
+    context={}
+    return render(request, 'app/cart.html', context)
+def checkout(request):
+    context={}
+    return render(request, 'app/checkout.html', context)
 
     
