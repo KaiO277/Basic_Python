b0b006606c355877543eec4c4a74f9fe38e4969e
KaiO277 <nghiatran1527@gmail.com>
2023-06-24 08:19:29 +0700

    KaiO

---
+++ TryHard/Array/hogwarts.py
@@ -0,0 +1,9 @@
+students = ["KaiO", "Nghia", "K.O"]
+
+for student in students:
+    print(student)
+
+print("\n#####\n")
+
+for i in range(len(students)):
+    print(students[i])
---
+++ TryHard/Bai1/cat.py
@@ -0,0 +1,30 @@
+# use loop
+
+# use while
+
+# i = 3
+# while i!=0:
+#     print("Meow")
+#     i -= 1 
+
+i = 1
+while i <= 3:
+    print("Meow")
+    i += 1
+
+
+print("use for")
+
+# use for
+
+for i in [0,1, 2]:
+    print("Meow")
+
+
+# not loop
+print("Meow\n" *3)
+
+n = int(input("what's n? "))
+
+for _ in range(n):
+    print(_)
