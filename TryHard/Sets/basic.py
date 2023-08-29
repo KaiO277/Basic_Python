4af186aeb931d11ebbd427153045477223b687c1
KaiO277 <nghiatran1527@gmail.com>
2023-08-29 18:18:46 +0700

    KaiO

---
+++ TryHard/Sets/basic.py
@@ -0,0 +1,12 @@
+# danh sách sẽ được in ra ngẫu nhiên để làm mới liên tục
+
+thisSet = {'red', 'blue', 'green'}
+print(thisSet)
+
+#và danh sách không có giá trị trùng lặp
+thisSet1 = {'red', 'blue', 'green', 'red'}
+print(thisSet1)
+
+# giá trị 1 và True được xem là giống nhau
+thisSet2 = {'red', 'blue', 'green', 'red', True, 1 , 2, 3}
+print(thisSet2)
