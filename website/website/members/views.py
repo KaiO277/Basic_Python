3e4861439a2486500bb940aaec831a2bd423a158
KaiO277 <nghiatran1527@gmail.com>
2023-09-13 17:53:24 +0700

    KaiO

--- website/website/members/views.py
+++ website/website/members/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-  template = loader.get_template('myfirst.html')
+  template = loader.get_template('index.html')
   return HttpResponse(template.render())
 
 def rooms(request):
