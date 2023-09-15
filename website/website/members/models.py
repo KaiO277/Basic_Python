f012115422478e4440a4cbaedf8728a2e7415510
KaiO277 <nghiatran1527@gmail.com>
2023-09-15 16:22:49 +0700

    KaiO

--- website/website/members/models.py
+++ website/website/members/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+import uuid
+from datetime import datetime
+ 
 User = get_user_model()
 
 # Create your models here.
@@ -13,6 +15,17 @@ class Profile(models.Model):
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return self.user.username 
+        return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user =  models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TimeField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
 
 
