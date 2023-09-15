5afeea9e1b6e2752453ad0933f5bc8b095ac4f55
KaiO277 <nghiatran1527@gmail.com>
2023-09-15 07:31:13 +0700

    KaiO

--- website/website/members/urls.py
+++ website/website/members/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signin/', views.signin, name='signin')   
+    path('signin', views.signin, name='signin'),   
+    path('signup', views.signup, name='signup'),   
 ]
