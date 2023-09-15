dbf59537af79645a3938552ae9309f30da997192
KaiO277 <nghiatran1527@gmail.com>
2023-09-15 09:57:25 +0700

    KaiO

--- website/website/members/urls.py
+++ website/website/members/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signin/', views.signin, name='signin'),   
-    path('signup/', views.signup, name='signup'),   
+    path('signup/', views.signup, name='signup'),  
+    path('logout/', views.logout, name='logout'),   
 ]
--- website/website/members/views.py
+++ website/website/members/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.template import loader
 from .models import Profile
 
 
+@login_required(login_url='signin')
 def index(request):
   # template = loader.get_template('index.html')
   # return HttpResponse(template.render())
   return render(request, 'index.html')
 
 def signin(request):
-  return render(request, 'signin.html')
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = auth.authenticate(username= username, password=password)
+
+    if user is not None:
+      auth.login(request,user)
+      return redirect('/')
+    else:
+      messages.info(request, "fail")
+      return redirect('signin')
+  
+  else:
+    return render(request, 'signin.html')
 
 def signup(request):
 
@@ -44,3 +59,8 @@ def signup(request):
     
   else:
     return render(request, 'signup.html')
+  
+@login_required(login_url='signin')  
+def logout(request):
+  auth.logout(request)
+  return redirect(signin)
