90b3a77f380b65a586ffcc754a34b920a463d487
KaiO277 <nghiatran1527@gmail.com>
2023-09-16 17:03:18 +0700

    KaiO

--- website/website/members/models.py
+++ website/website/members/models.py
@@ -2,16 +2,15 @@ from django.db import models
 from django.contrib.auth import get_user_model
 import uuid
 from datetime import datetime
- 
+
 User = get_user_model()
 
 # Create your models here.
-
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images',default='blank_profile_picture.jpg')
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
@@ -21,7 +20,7 @@ class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user =  models.CharField(max_length=100)
     image = models.ImageField(upload_to='post_images')
-    caption = models.TimeField()
+    caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
 
--- website/website/members/views.py
+++ website/website/members/views.py
@@ -10,7 +10,9 @@ from django.http import HttpResponse
 def index(request):
   user_object = User.objects.get(username=request.user.username)
   user_profile = Profile.objects.get(user = user_object)
-  return render(request, 'index.html', {'user_profile':user_profile})
+
+  posts = Post.objects.all()
+  return render(request, 'index.html', {'user_profile':user_profile, 'posts':posts})
 
 @login_required(login_url='signin')
 def settings(request):
@@ -36,20 +38,17 @@ def settings(request):
       user_profile.bio = bio
       user_profile.location = location
       user_profile.save()
-    return redirect('setting')  
+    return redirect('settings')  
 
   return render(request, 'setting.html', {'user_profile': user_profile})
 
-@login_required(login_url='signin')
-def upload(request):
-  
-  return HttpResponse('<h1>Upload View</h1>')
+
 
 @login_required(login_url='signin')
 def upload(request):
   if request.method == 'POST':
     user = request.user.username
-    image = request.FILES.get('upload_image')
+    image = request.FILES.get('image_upload')
     caption = request.POST['caption']
     
     new_post = Post.objects.create(user=user, image=image, caption= caption)
@@ -78,38 +77,39 @@ def signin(request):
 
 def signup(request):
 
-  if request.method == 'POST':
-    username = request.POST['username']
-    email = request.POST['email']
-    password = request.POST['password']
-    password2 = request.POST['password2']
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
 
-    if password == password2:
-      if User.objects.filter(email=email).exists():
-        messages.info(request, "Email taken")
-        return redirect('signup')
-      elif User.objects.filter(username=username).exists():
-        messages.info(request, "Username taken")
-        return redirect('signup')
-      else:
-        user = User.objects.create_user(username=username, email=email, password=password)
-        user.save()
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
 
-        user_login = auth.authencation(username=username, password = password)
-        auth.login(request, user_login)
-        print(user)
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
-        user_model = User.objects.get(username=username)
-        new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
-        new_profile.save()
-        return redirect('signup')
-
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+        
     else:
-      messages.info(request, "Password not matching")
-      return redirect('signup')
-    
-  else:
-    return render(request, 'signup.html')
+        return render(request, 'signup.html')
+
   
 @login_required(login_url='signin')  
 def logout(request):
