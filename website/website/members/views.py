aacc4fbfb0ec572148273d5080c0c995fe312746
KaiO277 <nghiatran1527@gmail.com>
2023-09-17 10:32:14 +0700

    KaiO

--- website/website/members/views.py
+++ website/website/members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 from django.http import HttpResponse
 
 
@@ -42,7 +42,26 @@ def settings(request):
 
   return render(request, 'setting.html', {'user_profile': user_profile})
 
+def like_post(request):
+   username = request.user.username
+   post_id = request.GET.get('post_id')
 
+   post = Post.objects.get(id=post_id) 
+
+   like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+   if like_filter == None:
+      new_like = LikePost.objects.create(post_id=post_id, username=username)
+      new_like.save()
+      post.no_of_likes = post.no_of_likes+1
+      post.save()
+      return redirect('/')
+   else:
+      like_filter.delete()   
+      post.no_of_likes = post.no_of_likes-1
+      post.save()
+      return redirect('/')
+   
 
 @login_required(login_url='signin')
 def upload(request):
