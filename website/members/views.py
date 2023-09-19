263e2f221cb5be734d83c1c60343c0bcf0ff30b1
KaiO277 <nghiatran1527@gmail.com>
2023-09-19 07:38:10 +0700

    KaiO

--- website/members/views.py
+++ website/members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 from django.http import HttpResponse
 
 
@@ -80,7 +80,20 @@ def profile(request, pk):
 
 @login_required(login_url='signin')
 def follow(request):
-   pass 
+   if request.method == 'POST':
+      follower = request.POST['followers']
+      user = request.POST['user']
+
+      if FollowersCount.objects.filter(follower=follower, user=user).first():
+         delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+         delete_follower.delete()
+         return redirect('/profile/'+user)
+      else:
+         new_follower = FollowersCount.objects.create(follower=follower, user=user)
+         new_follower.save()
+         return redirect('/profile/'+user)
+   else:
+      return redirect('/') 
 
 @login_required(login_url='signin')
 def upload(request):
